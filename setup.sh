ab5eeab8b565426e77d32564ec9047f3ed06017c
Yassine Alaoui bichr <yaalaoui@e2r13p6.1337.ma>
2021-01-13 18:44:09 +0100

    cleanse but not really

--- setup.sh
+++ setup.sh
@@ -1,20 +1,12 @@
-shopt -s expand_aliases
-source ~/.zshrc
-# sed -i '' "s/$(minikube ip)/MINIKUBE_IP/g" srcs/minikube_setup/metallb.yaml
-# sed -i '' "s/$(minikube ip)/'MINIKUBE_IP'/g" srcs/mysql/wp_db.sql
 minikube delete
 minikube start
 eval $(minikube docker-env)
-# minikube addons enable metallb
 docker pull metallb/speaker:v0.9.5
 docker pull metallb/controller:v0.9.5
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# sed -i '' "s/MINIKUBE_IP/$(minikube ip)/g" srcs/minikube_setup/metallb.yaml
-# sed -i '' "s/'MINIKUBE_IP'/$(minikube ip)/g" srcs/mysql/wp_db.sql
-kubectl create -f srcs/minikube_setup/metallb.yaml
+# kubectl create -f srcs/minikube_setup/metallb.yaml
 docker build -t d_mysql srcs/mysql/
 docker build -t d_wordpress srcs/wordpress/
 docker build -t d_phpmyadmin srcs/phpmyadmin/
@@ -23,8 +15,3 @@ docker build -t d_influxdb srcs/influxdb/
 docker build -t d_grafana srcs/grafana/
 docker build -t d_ftps srcs/ftps/
 kubectl apply -f srcs/minikube_setup/
-# kubectl create -f srcs/minikube_setup/influxdb-deployment.yaml
-# kubectl create -f srcs/minikube_setup/nginx-deployment.yaml
-# kubectl create -f srcs/minikube_setup/wordpress-deployment.yaml
-# kubectl create -f srcs/minikube_setup/phpmyadmin-deployment.yaml
-# kubectl create -f srcs/minikube_setup/grafana-deployment.yaml
